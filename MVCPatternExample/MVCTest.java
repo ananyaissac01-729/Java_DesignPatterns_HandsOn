eee47f98467206bda5fd05968b862e6491a84ecc
ananyaissac01-729 <ananyaissac01@gmail.com>
2026-06-26 19:21:55 +0530

    Add MVC pattern exercise

---
+++ MVCPatternExample/MVCTest.java
@@ -0,0 +1,14 @@
+public class MVCTest {
+    public static void main(String[] args) {
+        Student student = new Student("Suzza", "VJ101", "A");
+        StudentView view = new StudentView();
+
+        StudentController controller = new StudentController(student, view);
+
+        controller.updateView();   
+
+        controller.setStudentGrade("A+");
+
+        controller.updateView();  
+    }
+}
